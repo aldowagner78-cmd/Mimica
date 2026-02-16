164e63548b315a628cb18b9afcc5d3925a7dc8a3
Aldo Wagner <aldowagner78@gmail.com>
2026-02-16 18:27:43 -0300

    Added PWA support (manifest and sw)

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'mimica-pro-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './gameData.js',
+    './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => res || fetch(e.request))
+    );
+});
